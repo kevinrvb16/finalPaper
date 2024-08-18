a2eb6e86e31e02d5838b91ce7738c1e0af1444cd
KEVIN RAFAEL VELEZ BERNAL <42079591+kevinrvb16@users.noreply.github.com>
2024-08-18 16:32:06 -0300

    Adjust groups of metrics

--- src/pages/Metrics.vue
+++ src/pages/Metrics.vue
@@ -8,8 +8,8 @@
         <h1 class="text-h2 font-weight-bold">Grupos de Métricas</h1>
       </div>
       <v-row justify="end" >
-        <v-col class="pb-0" cols="4" v-for="(metric, index) in metrics" :key="index" justify="center" align="center">
-            <flip-card :title="metric.title" :description="metric.description" :color="metric.backgroundColor"></flip-card>
+        <v-col class="pb-0" cols="4" v-for="(metricGroup, index) in metrics" :key="index" justify="center" align="center">
+            <flip-card @click="selectedMetrics.push(metricGroup)" :title="metricGroup.title" :description="metricGroup.description" :color="metricGroup.backgroundColor"></flip-card>
         </v-col>
         <v-col cols="4" justify="center" align="center" class="my-auto">
           <v-btn append-icon="mdi-chevron-double-right" @click="redirect()">Avançar</v-btn>
@@ -49,32 +49,32 @@ export default {
           backgroundColor: "#000000" // Preto
         },
         {
-          title: "Desempenho do Processo",
-          value: "process",
+          title: "Desempenho e Processo",
+          value: "performance_and_process",
           description: "Se concentra na eficiência e eficácia dos processos usados no projeto, incluindo a eficácia dos métodos de gerenciamento de projetos, a qualidade dos processos de produção, a produtividade da equipe e a eficácia da comunicação interna.",
           backgroundColor: "#1C4E8A" // Azul
         },
         {
           title: "Cronograma e Progresso",
-          value: "progress",
+          value: "schedule_and_progress",
           description: "Se concentra no cronograma e no progresso do projeto, incluindo o cumprimento de prazos, o acompanhamento do progresso e a identificação e correção de atrasos.",
           backgroundColor: "#B971CA" // Roxo
         },
         {
           title: "Eficácia da Tecnologia",
-          value: "efficacy",
+          value: "technology_effectiveness",
           description: "Se concentra na eficácia das tecnologias usadas no projeto, incluindo a eficácia dos sistemas de informação, a eficácia dos softwares e a segurança e privacidade dos dados.",
           backgroundColor: "#4F8A10" // Verde
         },
         {
           title: "Tamanho e Estabilidade",
-          value: "stability",
+          value: "size_and_stability",
           description: "Se concentra no tamanho e estabilidade do produto ou sistema desenvolvido, incluindo o número de usuários, a capacidade de armazenamento, a escalabilidade e a estabilidade.",
           backgroundColor: "#F0803C" // Laranja
         },
         {
           title: "Satisfação do Cliente",
-          value: "custumer",
+          value: "customer_satisfaction",
           description: "Se concentra na satisfação do cliente com o produto ou serviço entregue, incluindo a qualidade, a usabilidade, a confiabilidade e a experiência geral do usuário. Isso pode ser medido por meio de pesquisas de satisfação do cliente e avaliações de clientes.",
           backgroundColor: "#6CAF44" // Verde Claro
         }
@@ -85,7 +85,7 @@ export default {
     redirect() {
       this.$router.push({
         path: '/table',
-        query: { metrics: JSON.stringify(this.selectedMetrics)}
+        query: { metricGroup: JSON.stringify(this.selectedMetrics)}
       })
     }
   },
