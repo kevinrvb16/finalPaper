78ea5e01d75f404c9f6992bc1f78317b04fc0aac
KEVIN RAFAEL VELEZ BERNAL <42079591+kevinrvb16@users.noreply.github.com>
2024-09-21 20:13:41 -0300

    Login with google then redirect page

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -18,18 +18,20 @@
 
       <div class="py-4" />
       <v-row class="mb-4" justify="center" align="center">
-        <div id="g_id_onload"
+        <div
+          id="g_id_onload"
           data-client_id="266301269895-9iap0cpovgq5fju28e6q6a87qudv2is0.apps.googleusercontent.com"
           data-context="signin"
           data-ux_mode="popup"
-          data-login_uri="https://metricspoker.netlify.app/"
-          data-itp_support="true">
+          data-callback="handleSignInWithGoogle"
+          data-itp_support="true"
+          data-use_fedcm_for_prompt="true">
         </div>
 
         <div class="g_id_signin"
           data-type="standard"
           data-shape="pill"
-          data-theme="filled_black"
+          data-theme="outline"
           data-text="signin_with"
           data-size="large"
           data-logo_alignment="left"
@@ -39,3 +41,45 @@
     </v-responsive>
   </v-container>
 </template>
+
+<script>
+import { supabase } from '../main'
+
+export default {
+  setup() {
+    async function handleSignInWithGoogle(response) {
+      try {
+        // send id token returned in response.credential to supabase
+        const { data, error } = await supabase.auth.signInWithIdToken({
+          provider: 'google',
+          token: response.credential,
+        })
+
+        if (error) throw error
+        console.log('Session data: ', data)
+        console.log('Successfully logged in with Google One Tap')
+
+        // redirect to protected page
+        this.$router.push('/rules')
+      } catch (error) {
+        console.error('Error logging in with Google One Tap', error)
+      }
+    }
+
+    return {
+      handleSignInWithGoogle
+    }
+  },
+  mounted() {
+    const { data, error } = supabase.auth.getSession()
+    if (error) {
+      console.error('Não está logado ou deu erro de sessão', error)
+    }
+    if (data?.session) {
+      this.$router.push('/rules')
+      return
+    }
+  },
+}
+</script>
+
