eb715a93538a56052d49df32d71a99126be0edc0
KEVIN RAFAEL VELEZ BERNAL <42079591+kevinrvb16@users.noreply.github.com>
2024-09-27 20:38:57 -0300

    Add headerApp to Games page

--- src/pages/Games.vue
+++ src/pages/Games.vue
@@ -5,87 +5,51 @@
       class="align-centerfill-height mx-auto"
       max-width="1200"
     >
-        <v-alert
+        <v-snackbar
           text="Sessão de jogo iniciada com sucesso"
           title="Sucesso no login"
-          type="success"
-          icon="$success"
-          closable
-          variant="flat"
-          v-if="successEmail"
-        ></v-alert>
+          timer
+          color="success"
+          timeout="5000"
+          v-model="successEmail"
+          rounded="pill"
+        ></v-snackbar>
       <div class="text-center">
-        <h1 class="text-h2 mt-4 font-weight-bold">Regras</h1>
+        <h1 class="text-h2 mt-4 font-weight-bold">Jogos recentes</h1>
       </div>
 
       <div class="py-4" />
-
-      <v-row justify="center" align="center">
-        <v-col cols="6" class="text-justify">
-          <v-card variant="tonal">
-            <v-card-title>
-              Importante que todos leiam com atenção as regras do jogo.
-            </v-card-title>
-            <v-card-subtitle>
-              A dinâmica inicia com a definição de quem será o Dealer.
-            </v-card-subtitle>
-            <v-card-text>
-              O Dealer questiona os participantes sobre eventuais dores,
-               ou seja, problemas que a equipe identifica ao desenvolver 
-               (caso a equipe não consiga identificar alguma dificuldade, 
-               o Dealer pode trazer dores previamente selecionadas).
-            </v-card-text>
-            <v-card-text>
-              Uma vez selecionadas duas dores, cada um dos jogadores 
-              consulta a tabela de grupos de métricas com suas 
-              respectivas definições. Os participantes devem fazer 
-              uma votação simples para selecionar 2 grupos de métricas 
-              (cada um escolhe 2, em seguida os 2 mais votados são escolhidos).
-            </v-card-text>
-            <v-card-text>
-              O Dealer seleciona os 2 grupos de métricas para que cada uma 
-              das metricas relacionadas a cada grupo apareçam para todos.
-            </v-card-text>
-          </v-card>
-        </v-col>
-        <v-col cols="6" class="text-justify">
-          <v-card variant="tonal">
-            <v-card-text>
-              É possivel inserir uma métrica personalizada da equipe. 
-              Para tanto deve-se clicar na carta "Joker" e inserir nome e descriçao.
-            </v-card-text>
-            <v-card-text>
-              Cada jogador pode clicar nas cartas das métricas para ler a descrição 
-              no verso de cada uma.
-            </v-card-text>
-            <v-card-text>
-              Cada participante recebe 3 fichas  de votação, cada um com um critério.
-            </v-card-text>
-            <v-card-text>
-              Sem mostrar para os outros participantes cada um vota nas cartas que achar pertinente.
-            </v-card-text>
-            <v-card-text>
-              As duas métricas que tiverem a maior quantidade, se houver empate a votação 
-              deve ocorrer novamente apenas com as métricas empatadas.
-            </v-card-text>
-            <v-card-text>
-              Em seguida é feito o mesmo procedimento com a seguinte dor.
-            </v-card-text>
-          </v-card>
-        </v-col>
-      </v-row>
-      <v-row justify="center" align="center">
-        <v-col cols="12" class="text-center">
-          <problems-dialog>Descrever dores</problems-dialog>
-        </v-col>
-      </v-row>
+      <v-table>
+        <thead>
+          <tr>
+            <th class="text-left">
+              Nome do jogo
+            </th>
+            <th class="text-left">
+              Dor
+            </th>
+            <th class="text-left">
+              Métricas Escolhidas
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            v-for="item in desserts"
+            :key="item.name"
+          >
+            <td>{{ item.name }}</td>
+            <td>{{ item.problem }}</td>
+            <td>{{ item.metricas[0] }}, {{ item.metricas[1] }}</td>
+          </tr>
+        </tbody>
+      </v-table>
     </v-responsive>
   </v-container>
 </template>
 
 <script>
 import { useVuelidate } from '@vuelidate/core';
-import ProblemsDialog from '@/components/ProblemsDialog.vue'
 import HeaderApp from '@/components/HeaderApp.vue'
 
 export default {
@@ -93,12 +57,18 @@ export default {
     return { v$: useVuelidate() }
   },
   components: {
-    ProblemsDialog,
     HeaderApp
   },
   data() {
     return {
       successEmail: true,
+      desserts: [
+        {
+          name: 'Jogo',
+          problem: 'UX - Usabilidade',
+          metricas: ['Qualidade', 'Relevância']
+        },
+      ],
     }
   },
 }
