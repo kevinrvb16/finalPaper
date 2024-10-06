b2a8b6adf86ae2e4d25c9484552498ce308d6527
KEVIN RAFAEL VELEZ BERNAL <42079591+kevinrvb16@users.noreply.github.com>
2024-10-06 15:55:52 -0300

    add actions

--- src/pages/Games.vue
+++ src/pages/Games.vue
@@ -39,7 +39,7 @@
             <th id="created_at" class="text-left">
               Data de criação
             </th>
-            <th id="links" class="text-left">Ações</tr>
+            <th id="links" class="text-left">Ações</th>
         </thead>
         <tbody>
           <tr
