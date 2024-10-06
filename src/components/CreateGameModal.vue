e57c96eb1e0f45efb415f3bce40c8d9613a82f57
KEVIN RAFAEL VELEZ BERNAL <42079591+kevinrvb16@users.noreply.github.com>
2024-10-06 14:56:06 -0300

    create modal game user

---
+++ src/components/CreateGameModal.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-dialog max-width="480">
+  <template v-slot:activator="{ props: activatorProps }">
+    <v-btn
+      v-bind="activatorProps"
+      text="Criar novo jogo"
+      color="primary"
+    ></v-btn>
+  </template>
+
+  <template v-slot:default="{ isActive }">
+    <v-card title="Criar jogo:">
+      <div class="mx-5">
+        <v-text-field label="Digite o nome do jogo" variant="solo-filled" ></v-text-field>
+      </div>
+      <v-spacer></v-spacer>
+      <v-card-actions>
+        <v-btn
+          variant="outlined"
+          text="Criar"
+          class="mx-auto mb-6"
+          color="primary"
+          @click="close(isActive)"
+        ></v-btn>
+      </v-card-actions>
+    </v-card>
+  </template>
+</v-dialog>
+</template>
+
+<script>
+import { useVuelidate } from '@vuelidate/core';
+import { required } from '@vuelidate/validators'
+
+export default {
+  setup() {
+    return { v$: useVuelidate() }
+  },
+  props: {
+    loading: {
+        type: Boolean
+    }
+  },
+  data() {
+    return {
+      problems: []
+    }
+  },
+  validations() {
+    return {
+      problems: { required }
+    }
+  },
+  methods: {
+    close(isActive) {
+        this.$emit('create')
+      isActive.value = false
+    }
+  },
+}
+</script>
