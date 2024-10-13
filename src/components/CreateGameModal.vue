0f3e5ee9c480b77c452bdca6ac231939375f13d2
KEVIN RAFAEL VELEZ BERNAL <42079591+kevinrvb16@users.noreply.github.com>
2024-10-13 14:55:17 -0300

    vendo se vai validar

--- src/components/CreateGameModal.vue
+++ src/components/CreateGameModal.vue
@@ -24,6 +24,7 @@
           text="Criar"
           class="mx-auto mb-6"
           color="primary"
+          :disabled="!$v.valid"
           @click="save(isActive)"
         ></v-btn>
       </v-card-actions>
