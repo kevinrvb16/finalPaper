f9ecc50f98b51b64c9a74d269abc9403140d0fb8
KEVIN RAFAEL VELEZ BERNAL <42079591+kevinrvb16@users.noreply.github.com>
2024-10-15 22:04:24 -0300

    Remove console log

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-console.log(supabaseUrl)
+
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
