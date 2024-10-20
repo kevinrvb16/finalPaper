c1f2e0db1caef57a872d177ee40501c647e68dfb
kevinrvb16 <kevinrvb16@gmail.com>
2024-10-20 15:16:32 -0300

    Melhorando log

--- src/pages/Metrics.vue
+++ src/pages/Metrics.vue
@@ -140,7 +140,8 @@ export default {
         console.log('data createAnonUser', data)
         console.log(this.anonUser)
         this.noAnonUser = false;
-      } else if (error){
+      }
+      if (error){
         console.log('error CreateAnonUser', error)
       }
     }
