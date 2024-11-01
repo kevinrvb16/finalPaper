db362cc71ac8b83e113c05de4b905f2658118642
kevinrvb16 <kevinrvb16@gmail.com>
2024-11-01 20:54:35 -0300

    adjust layout to create problems

--- src/components/CreateGameModal.vue
+++ src/components/CreateGameModal.vue
@@ -14,10 +14,10 @@
         <v-text-field :error="v$.name.$error" v-model="name" label="Digite o nome do jogo" variant="solo-filled" ></v-text-field>
         <div class="d-flex">
           <v-text-field density="compact" :error="v$.problemA.name.$error" class="mr-3" v-model="problemA.name" label="Digite o nome da dor 1" variant="solo-filled"></v-text-field>
-          <v-textarea max-rows="5" rows="3" :error="v$.problemA.description.$error" v-model="problemA.description" label="Digite a descrição da dor 1" variant="solo-filled"></v-textarea>
+          <v-text-field :error="v$.problemB.name.$error" class="mr-3" v-model="problemB.name" label="Digite o nome da dor 2" variant="solo-filled"></v-text-field>
         </div>
         <div class="d-flex">
-          <v-text-field :error="v$.problemB.name.$error" class="mr-3" v-model="problemB.name" label="Digite o nome da dor 2" variant="solo-filled"></v-text-field>
+          <v-textarea max-rows="5" rows="3" :error="v$.problemA.description.$error" v-model="problemA.description" label="Digite a descrição da dor 1" variant="solo-filled"></v-textarea>
           <v-textarea max-rows="5" rows="3" :error="v$.problemB.description.$error" v-model="problemB.description" label="Digite a descrição da dor 2" variant="solo-filled"></v-textarea>
         </div>
       </div>
