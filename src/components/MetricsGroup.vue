9598558cbb0264f4d58ffac6d782a99b0cbed70e
kevinrvb16 <kevinrvb16@gmail.com>
2024-11-03 14:59:20 -0300

    mudando componente seleção grupo de métricas

---
+++ src/components/MetricsGroup.vue
@@ -0,0 +1,87 @@
+<template>
+    <v-col class="pb-0" cols="3" v-for="(metricGroup, index) in metrics" :key="index" justify="center"
+          align="center">
+        <v-card
+            :color="metricGroup.backgroundColor"
+            variant="tonal"
+            class="mx-auto"
+            @click="selectGroup(metricGroup)"
+            hover
+            link
+            >
+            <v-card-item>
+                <div>
+                <div class="text-overline mb-1">
+                    {{ metricGroup.title }}
+                </div>
+                <v-card-subtitle>{{ metricGroup.description }}</v-card-subtitle>
+                </div>
+            </v-card-item>
+
+            <v-card-actions>
+                <v-btn>
+                Button
+                </v-btn>
+            </v-card-actions>
+        </v-card>
+    </v-col>
+</template>
+<script>
+
+export default {
+    data() {
+        return {
+            problems: [],
+            metrics: [
+                {
+                title: "Qualidade do Produto",
+                value: "product_quality",
+                description: "Se concentra na qualidade do produto ou serviço entregue, incluindo sua funcionalidade, confiabilidade, segurança, facilidade de uso, desempenho, manutenção, entre outros aspectos.",
+                backgroundColor: "#B90000" // Vermelho
+                },
+                {
+                title: "Recursos e Custos",
+                value: "resources",
+                description: "Se concentra nos recursos usados no projeto e no custo total do projeto, incluindo o orçamento e o uso de materiais ou ferramentas.",
+                backgroundColor: "#000000" // Preto
+                },
+                {
+                title: "Desempenho e Processo",
+                value: "performance_and_process",
+                description: "Se concentra na eficiência e eficácia dos processos usados no projeto, incluindo a eficácia dos métodos de gerenciamento de projetos, a qualidade dos processos de produção, a produtividade da equipe e a eficácia da comunicação interna.",
+                backgroundColor: "#1C4E8A" // Azul
+                },
+                {
+                title: "Cronograma e Progresso",
+                value: "schedule_and_progress",
+                description: "Se concentra no cronograma e no progresso do projeto, incluindo o cumprimento de prazos, o acompanhamento do progresso e a identificação e correção de atrasos.",
+                backgroundColor: "#B971CA" // Roxo
+                },
+                {
+                title: "Eficácia da Tecnologia",
+                value: "technology_effectiveness",
+                description: "Se concentra na eficácia das tecnologias usadas no projeto, incluindo a eficácia dos sistemas de informação, a eficácia dos softwares e a segurança e privacidade dos dados.",
+                backgroundColor: "#4F8A10" // Verde
+                },
+                {
+                title: "Tamanho e Estabilidade",
+                value: "size_and_stability",
+                description: "Se concentra no tamanho e estabilidade do produto ou sistema desenvolvido, incluindo o número de usuários, a capacidade de armazenamento, a escalabilidade e a estabilidade.",
+                backgroundColor: "#F0803C" // Laranja
+                },
+                {
+                title: "Satisfação do Cliente",
+                value: "customer_satisfaction",
+                description: "Se concentra na satisfação do cliente com o produto ou serviço entregue, incluindo a qualidade, a usabilidade, a confiabilidade e a experiência geral do usuário. Isso pode ser medido por meio de pesquisas de satisfação do cliente e avaliações de clientes.",
+                backgroundColor: "#6CAF44" // Verde Claro
+                }
+            ]
+        }
+    },
+    props: {
+
+    },
+}
+</script>
+<style>
+</style>
--- src/pages/Game.vue
+++ src/pages/Game.vue
@@ -11,11 +11,7 @@
         <h3>Selecione 2 grupos de métricas:</h3>
       </div>
       <v-row justify="end">
-        <v-col class="pb-0" cols="4" v-for="(metricGroup, index) in metrics" :key="index" justify="center"
-          align="center">
-          <flip-card @click="selectedMetrics.push(metricGroup)" :title="metricGroup.title"
-            :description="metricGroup.description" :color="metricGroup.backgroundColor"></flip-card>
-        </v-col>
+        <metrics-group></metrics-group>
         <v-col cols="4" justify="center" align="center" class="my-auto">
           <v-btn v-if="isDealer" append-icon="mdi-chevron-double-right" @click="redirect()">Avançar</v-btn>
         </v-col>
@@ -94,6 +90,7 @@ import { useVuelidate } from '@vuelidate/core';
 import FlipCard from '@/components/FlipCard.vue';
 import HeaderApp from '@/components/HeaderApp.vue'
 import { required } from '@vuelidate/validators'
+import MetricsGroup from '@/components/MetricsGroup.vue';
 
 export default {
   setup() {
@@ -101,7 +98,8 @@ export default {
   },
   components: {
     FlipCard,
-    HeaderApp
+    HeaderApp,
+    MetricsGroup
   },
   data() {
     return {
@@ -114,50 +112,6 @@ export default {
       participants: [],
       problem: null,
       id: JSON.parse(this?.$route.query.id),
-      metrics: [
-        {
-          title: "Qualidade do Produto",
-          value: "product_quality",
-          description: "Se concentra na qualidade do produto ou serviço entregue, incluindo sua funcionalidade, confiabilidade, segurança, facilidade de uso, desempenho, manutenção, entre outros aspectos.",
-          backgroundColor: "#B90000" // Vermelho
-        },
-        {
-          title: "Recursos e Custos",
-          value: "resources",
-          description: "Se concentra nos recursos usados no projeto e no custo total do projeto, incluindo o orçamento e o uso de materiais ou ferramentas.",
-          backgroundColor: "#000000" // Preto
-        },
-        {
-          title: "Desempenho e Processo",
-          value: "performance_and_process",
-          description: "Se concentra na eficiência e eficácia dos processos usados no projeto, incluindo a eficácia dos métodos de gerenciamento de projetos, a qualidade dos processos de produção, a produtividade da equipe e a eficácia da comunicação interna.",
-          backgroundColor: "#1C4E8A" // Azul
-        },
-        {
-          title: "Cronograma e Progresso",
-          value: "schedule_and_progress",
-          description: "Se concentra no cronograma e no progresso do projeto, incluindo o cumprimento de prazos, o acompanhamento do progresso e a identificação e correção de atrasos.",
-          backgroundColor: "#B971CA" // Roxo
-        },
-        {
-          title: "Eficácia da Tecnologia",
-          value: "technology_effectiveness",
-          description: "Se concentra na eficácia das tecnologias usadas no projeto, incluindo a eficácia dos sistemas de informação, a eficácia dos softwares e a segurança e privacidade dos dados.",
-          backgroundColor: "#4F8A10" // Verde
-        },
-        {
-          title: "Tamanho e Estabilidade",
-          value: "size_and_stability",
-          description: "Se concentra no tamanho e estabilidade do produto ou sistema desenvolvido, incluindo o número de usuários, a capacidade de armazenamento, a escalabilidade e a estabilidade.",
-          backgroundColor: "#F0803C" // Laranja
-        },
-        {
-          title: "Satisfação do Cliente",
-          value: "customer_satisfaction",
-          description: "Se concentra na satisfação do cliente com o produto ou serviço entregue, incluindo a qualidade, a usabilidade, a confiabilidade e a experiência geral do usuário. Isso pode ser medido por meio de pesquisas de satisfação do cliente e avaliações de clientes.",
-          backgroundColor: "#6CAF44" // Verde Claro
-        }
-      ]
     }
   },
   async mounted() {
