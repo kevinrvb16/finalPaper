aa27da63c79dda0fd902ede4b73cd5fd95bf8b5d
kevinrvb16 <kevinrvb16@gmail.com>
2024-11-03 18:59:10 -0300

    height card

--- src/components/MetricsGroup.vue
+++ src/components/MetricsGroup.vue
@@ -8,7 +8,7 @@
             hover
             link
             density="compact"
-            min-height="33vh"
+            min-height="40vh"
             :disabled="selectedGroups.length > 1 && !selectedGroups.includes(metricGroup)"
             :elevation="!selectedGroups.includes(metricGroup) ? 0 : 10"
             >
