d54f15c8b69ddd40b2381d3df56b3e765d47de5e
kevinrvb16 <kevinrvb16@gmail.com>
2024-11-04 18:48:52 -0300

    Disabled after choose btn enviar

--- src/pages/Game.vue
+++ src/pages/Game.vue
@@ -27,7 +27,7 @@
           <v-row class="pa-0" align="center">
             <metrics-group :isDealer="isDealer" @input="setSelectedGroups" :alreadyChoose="problemsSaved"></metrics-group>
             <v-col v-if="!isDealer" cols="3" justify="center" align="center">
-              <v-btn append-icon="mdi-chevron-double-right" @click="send">enviar</v-btn>
+              <v-btn :disabled="problemsSaved" append-icon="mdi-chevron-double-right" @click="send">enviar</v-btn>
               <div v-if="problemsSaved">Enviado com sucesso, aguarde o Dealer avançar.</div>
             </v-col>
           </v-row>
