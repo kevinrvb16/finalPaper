919d03a0fe825b45e6472c000325fc3b8869c7c8
kevinrvb16 <kevinrvb16@gmail.com>
2024-11-05 18:10:59 -0300

    metricsGroup ok

--- src/components/MetricsGroup.vue
+++ src/components/MetricsGroup.vue
@@ -1,5 +1,5 @@
 <template>
-    <v-col class="pb-0 px-1" :cols="small? '' : '3'" cols-xs="6" v-for="(metricGroup, index) in metricsGroup" :key="index" justify="center">
+    <v-col class="pb-0 px-1" :cols="small? '' : '3'" cols-xs="6" v-for="(metricGroup, index) in getMetricsGroup" :key="index" justify="center">
         <v-card
             :color="metricGroup.backgroundColor"
             variant="tonal"
