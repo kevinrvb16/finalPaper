4addca3b569ca9ec1973f10cbecd04ee2aa6a8d9
kevinrvb16 <kevinrvb16@gmail.com>
2024-11-05 21:55:29 -0300

    color

--- src/db/metricsGroup.js
+++ src/db/metricsGroup.js
@@ -1,52 +1,58 @@
 const metricsGroupList =  [
     {
-    title: "Qualidade do Produto",
-    value: "product_quality",
-    description: "Se concentra na qualidade do produto ou serviço entregue, incluindo sua funcionalidade, confiabilidade, segurança, facilidade de uso, desempenho, manutenção, entre outros aspectos.",
-    backgroundColor: "#B90000", // Vermelho
-    backgroundColorRGBA: "rgba(185, 0, 0, 0.5185"
+        title: "Qualidade do Produto",
+        value: "product_quality",
+        description: "Se concentra na qualidade do produto ou serviço entregue, incluindo sua funcionalidade, confiabilidade, segurança, facilidade de uso, desempenho, manutenção, entre outros aspectos.",
+        backgroundColor: "#B90000", // Vermelho
+        backgroundColorRGBA: "rgba(185, 0, 0, 0.185)",
+        color: "#8B0000" // Vermelho mais escuro
     },
     {
-    title: "Recursos e Custos",
-    value: "resources",
-    description: "Se concentra nos recursos usados no projeto e no custo total do projeto, incluindo o orçamento e o uso de materiais ou ferramentas.",
-    backgroundColor: "#000000", // Preto
-    backgroundColorRGBA: "rgba(0, 0, 0, 0.5185"
+        title: "Recursos e Custos",
+        value: "resources",
+        description: "Se concentra nos recursos usados no projeto e no custo total do projeto, incluindo o orçamento e o uso de materiais ou ferramentas.",
+        backgroundColor: "#000000", // Preto
+        backgroundColorRGBA: "rgba(0, 0, 0, 0.185)",
+        color: "#333333" // Cinza escuro
     },
     {
-    title: "Desempenho e Processo",
-    value: "performance_and_process",
-    description: "Se concentra na eficiência e eficácia dos processos usados no projeto, incluindo a eficácia dos métodos de gerenciamento de projetos, a qualidade dos processos de produção, a produtividade da equipe e a eficácia da comunicação interna.",
-    backgroundColor: "#1C4E8A", // Azul
-    backgroundColorRGBA: "rgba(28, 78, 138, 0.5185"
+        title: "Desempenho e Processo",
+        value: "performance_and_process",
+        description: "Se concentra na eficiência e eficácia dos processos usados no projeto, incluindo a eficácia dos métodos de gerenciamento de projetos, a qualidade dos processos de produção, a produtividade da equipe e a eficácia da comunicação interna.",
+        backgroundColor: "#1C4E8A", // Azul
+        backgroundColorRGBA: "rgba(28, 78, 138, 0.185)",
+        color: "#1c4c8c" // Azul mais escuro
     },
     {
-    title: "Cronograma e Progresso",
-    value: "schedule_and_progress",
-    description: "Se concentra no cronograma e no progresso do projeto, incluindo o cumprimento de prazos, o acompanhamento do progresso e a identificação e correção de atrasos.",
-    backgroundColor: "#B971CA", // Roxo
-    backgroundColorRGBA: "rgba(185, 113, 202, 0.5185"
+        title: "Cronograma e Progresso",
+        value: "schedule_and_progress",
+        description: "Se concentra no cronograma e no progresso do projeto, incluindo o cumprimento de prazos, o acompanhamento do progresso e a identificação e correção de atrasos.",
+        backgroundColor: "#B971CA", // Roxo
+        backgroundColorRGBA: "rgba(185, 113, 202, 0.185)",
+        color: "#8A2BE2" // Roxo mais escuro
     },
     {
-    title: "Eficácia da Tecnologia",
-    value: "technology_effectiveness",
-    description: "Se concentra na eficácia das tecnologias usadas no projeto, incluindo a eficácia dos sistemas de informação, a eficácia dos softwares e a segurança e privacidade dos dados.",
-    backgroundColor: "#1B5E20", // Verde
-    backgroundColorRGBA: "rgba(27, 94, 32, 0.5185"
+        title: "Eficácia da Tecnologia",
+        value: "technology_effectiveness",
+        description: "Se concentra na eficácia das tecnologias usadas no projeto, incluindo a eficácia dos sistemas de informação, a eficácia dos softwares e a segurança e privacidade dos dados.",
+        backgroundColor: "#1B5E20", // Verde
+        backgroundColorRGBA: "rgba(27, 94, 32, 0.185)",
+        color: "#006400" // Verde mais escuro
     },
     {
-    title: "Tamanho e Estabilidade",
-    value: "size_and_stability",
-    description: "Se concentra no tamanho e estabilidade do produto ou sistema desenvolvido, incluindo o número de usuários, a capacidade de armazenamento, a escalabilidade e a estabilidade.",
-    backgroundColor: "#F0803C", // Laranja
-    backgroundColorRGBA: "rgba(240, 128, 60, 0.5185"
+        title: "Tamanho e Estabilidade",
+        value: "size_and_stability",
+        description: "Se concentra no tamanho e estabilidade do produto ou sistema desenvolvido, incluindo o número de usuários, a capacidade de armazenamento, a escalabilidade e a estabilidade.",
+        backgroundColor: "#FF8C00", // Laranja
+        backgroundColorRGBA: "rgba(255, 140, 0, 0.185)",
+        color: "#FF4500" // Laranja mais escuro
     },
     {
-    title: "Satisfação do Cliente",
-    value: "customer_satisfaction",
-    description: "Se concentra na satisfação do cliente com o produto ou serviço entregue, incluindo a qualidade, a usabilidade, a confiabilidade e a experiência geral do usuário. Isso pode ser medido por meio de pesquisas de satisfação do cliente e avaliações de clientes.",
-    backgroundColor: "#6CAF44", // Verde Claro
-    backgroundColorRGBA: "rgba(108, 175, 68, 0.185)"
+        title: "Satisfação do Cliente",
+        value: "customer_satisfaction",
+        description: "Se concentra na satisfação do cliente com o produto ou serviço entregue, incluindo a qualidade, a usabilidade, a confiabilidade e a experiência geral do usuário. Isso pode ser medido por meio de pesquisas de satisfação do cliente e avaliações de clientes.",
+        backgroundColor: "#6CAF44", // Verde Claro
+        backgroundColorRGBA: "rgba(108, 175, 68, 0.185)"
     }
 ]
 
