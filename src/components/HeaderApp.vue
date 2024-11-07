7c443e9f8e9ac5298d1dc7763c015d1a0cf98faf
kevinrvb16 <kevinrvb16@gmail.com>
2024-11-07 11:53:18 -0300

    muda header

--- src/components/HeaderApp.vue
+++ src/components/HeaderApp.vue
@@ -1,5 +1,5 @@
 <template>
-    <v-app-bar image="@/assets/header.jpeg" class="px-4" :elevation="2">
+    <v-app-bar image="@/assets/header.jpg" class="px-4" :elevation="2">
         <template slot:prepend>
             <v-app-bar-nav-icon></v-app-bar-nav-icon>
         </template>
