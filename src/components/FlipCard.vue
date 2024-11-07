0003df37671d47c0279cf5bdbc62bdb85a31e0ac
kevinrvb16 <kevinrvb16@gmail.com>
2024-11-07 16:55:14 -0300

    icones corretos nas cartas

--- src/components/FlipCard.vue
+++ src/components/FlipCard.vue
@@ -3,8 +3,8 @@
     <div :class="['flip-card-inner', { 'flipped': isFlipped }]">
       <v-card variant="outlined" :color="color" class="flip-card-front pa-0" outlined>
         <v-card-title :class="`${customClassTitle} text-overline mt-4`">{{ title }}</v-card-title>
-        <v-icon :icon="cardIcon" style="position: absolute; top: 0; left: 0; font-size: 24px; " :color="color" ></v-icon>
-        <v-icon :icon="cardIcon" style="position: absolute; bottom: 0; right: 0; font-size: 24px; " :color="color" ></v-icon>
+        <v-icon :icon="cardIcon" style="position: absolute; top: 5; left: 5; font-size: 24px; " :color="color" ></v-icon>
+        <v-icon :icon="cardIcon" style="position: absolute; bottom: 5; right: 5; font-size: 24px; " :color="color" ></v-icon>
         <v-icon :icon="cardIcon" style="position: absolute; top: 44%; left: 41%; font-size: 24px; " :color="color" ></v-icon>
       </v-card>
       <v-card variant="outlined" :color="color" class="flip-card-back pa-0" outlined>
