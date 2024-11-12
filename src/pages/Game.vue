0ecdf8e9d7402e7b79968d34f99d3487b1146bf8
kevinrvb16 <kevinrvb16@gmail.com>
2024-11-11 21:42:43 -0300

    ajustes

--- src/pages/Game.vue
+++ src/pages/Game.vue
@@ -135,8 +135,9 @@
         <v-btn v-if="isDealer" append-icon="mdi-chevron-double-right" @click="changeStatus">Iniciar Próxima Dor</v-btn>
       </div>
       <v-row no-gutters class="mb-3">
-        <v-col no-gutters cols="5" v-for="metric in twoMetricsSelected" :key="metric.metric">
+        <v-col no-gutters cols="5" v-for="metric in twoMetricsSelected" :key="metric.metric" class="align-center justify-center">
           <flip-card :id="metric.metric"  :cardIcon="'mdi-cards-diamond'" :customClassFlipCard="'custom-flip-card'" :customClassTitle="'white-space-normal'" :title="metric.name" :description="metric.description" color="black"></flip-card>
+          {{  metric.count }} votos
         </v-col>
         <v-col no-gutters cols="2" class="d-flex pl-2 align-center justify-end">
           <participants :gameId="id" @input="setParticipants"></participants>
